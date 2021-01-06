05b02ebe7a3b196b9f55b5007a3de099caf4863e
Richard Barker <74937909+richardbarkr@users.noreply.github.com>
2021-01-06 00:20:33 +0000

    Update Problem 4.py

--- PSET 4/Problem 4.py
+++ PSET 4/Problem 4.py
@@ -1,1 +1,11 @@
-# Problem 4: FILL IN MISSING CODE TO GENERATE y VALUESx1 = INTERVAL_1x2 = INTERVAL_2y = []# MISSING LINESmodels = generate_models(x1, y, [1])    evaluate_models_on_training(x1, y, models)for year in INTERVAL_1:    y.append(numpy.mean(raw_data.get_yearly_temp('BOSTON', year)))
+# Problem 4: FILL IN MISSING CODE TO GENERATE y VALUES
+x1 = INTERVAL_1
+x2 = INTERVAL_2
+y = []
+# MISSING LINES
+models = generate_models(x1, y, [1])    
+evaluate_models_on_training(x1, y, models)
+
+for year in INTERVAL_1:
+    y.append(numpy.mean(raw_data.get_yearly_temp('BOSTON', year)))
+    
